9c8c4608f56765cb32efe72c49b11c180b63e5b3
Iago Gondim <iago-gondim@hotmail.com>
2022-10-03 15:16:33 -0300

    scroll-easy-link-interno

--- script.js
+++ script.js
@@ -41,3 +41,24 @@ function initAccordion(){
 }
 
 initAccordion()
+
+
+function initScrollEasy(){
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+
+  function scrollToSection(event){
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href')
+    const section = document.querySelector(href)
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection)
+  })
+}
+
+initScrollEasy()
